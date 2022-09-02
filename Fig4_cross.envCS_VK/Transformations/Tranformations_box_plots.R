4fa1de66918c03b4e311e0c53def3daace712a3f
Vanessa Garayburu-Caruso <69696155+vgc40@users.noreply.github.com>
2022-09-02 13:32:36 -0700

    Update Tranformations_box_plots.R

--- Fig4_cross.envCS_VK/Transformations/Tranformations_box_plots.R
+++ Fig4_cross.envCS_VK/Transformations/Tranformations_box_plots.R
@@ -66,3 +66,47 @@ ggplot(df.mf, aes(x = cs.flag.emergent_overlap , fill = variable, y = value)) +
   theme(axis.title.y =element_text(size=8))
 ggsave(file="Transformation_mf_thresholds.pdf")
 
+
+ggplot(df.all, aes(fill = cs.flag.emergent_overlap , x = variable, y = value)) +
+  coord_cartesian() + geom_boxplot() +
+  stat_summary(fun.y = mean, geom = "point", shape = 18, size = 2, colour = "yellow",  position = position_dodge(width=0.75), alpha = 0.8)+
+  labs(x=expression()) +
+  labs(y = expression(Number~of~Transformations))+ 
+  # stat_compare_means(method="t-test",hide.ns = TRUE,method.args = list(alternative = "less"),label="p.signif", label.y=0)+
+  theme_bw()+theme(legend.position="top")+theme(
+    plot.background = element_blank()
+    ,panel.grid.major = element_blank()
+    ,panel.grid.minor = element_blank()  ) +
+  theme(legend.title = element_blank(),  legend.background = element_rect(fill = 'NA'), legend.text = element_text(size=8))+
+  #theme(legend.position=c(0.8,0.9))+
+  scale_fill_brewer(palette="Set2")+ 
+  theme(axis.text.x=element_text(colour = c("black","black")))+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  theme(axis.text.x=element_text(size=8))+
+  theme(axis.text.y=element_text(size=8))+
+  theme(axis.title.x =element_text(size=8))+
+  theme(axis.title =element_text(size=8))+
+  theme(axis.title.y =element_text(size=8))
+ggsave(file="Transformation_all_thresholds_v2.pdf")
+
+ggplot(df.mf, aes(fill = cs.flag.emergent_overlap , x = variable, y = value)) +
+  coord_cartesian() + geom_boxplot() +
+  stat_summary(fun.y = mean, geom = "point", shape = 18, size = 2, colour = "yellow",  position = position_dodge(width=0.75), alpha = 0.8)+
+  labs(x=expression()) +
+  labs(y = expression(Number~of~Transformations))+ 
+  # stat_compare_means(method="t-test",hide.ns = TRUE,method.args = list(alternative = "less"),label="p.signif", label.y=0)+
+  theme_bw()+theme(legend.position="top")+theme(
+    plot.background = element_blank()
+    ,panel.grid.major = element_blank()
+    ,panel.grid.minor = element_blank()  ) +
+  theme(legend.title = element_blank(),  legend.background = element_rect(fill = 'NA'), legend.text = element_text(size=8))+
+  #theme(legend.position=c(0.8,0.9))+
+  scale_fill_brewer(palette="Set2")+ 
+  theme(axis.text.x=element_text(colour = c("black","black")))+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  theme(axis.text.x=element_text(size=8))+
+  theme(axis.text.y=element_text(size=8))+
+  theme(axis.title.x =element_text(size=8))+
+  theme(axis.title =element_text(size=8))+
+  theme(axis.title.y =element_text(size=8))
+ggsave(file="Transformation_mf_thresholds_v2.pdf")
