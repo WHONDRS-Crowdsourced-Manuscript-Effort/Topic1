a9867bc1d06054e5c6b0e26fa46a84676c019836
Michaela de Melo <70396791+demelo-limno@users.noreply.github.com>
2022-10-05 08:38:52 -0400

    editions x axis

--- Fig5_cross.envCS_indices/Script_Fig.5_Boxplots.R
+++ Fig5_cross.envCS_indices/Script_Fig.5_Boxplots.R
@@ -60,13 +60,13 @@ plot2=ggboxplot(cross, x = "cs.flag.emergent_overlap" , y = "NOSC", fill = "cs.f
 
 plot3=ggboxplot(cross, x = "cs.flag.emergent_overlap" , y = "DBE", fill = "cs.flag.emergent_overlap") +
   #stat_pvalue_manual(stat.test, hide.ns = FALSE) +
-  theme_bw() +theme(legend.position="none", axis.text.x=element_text(angle=90), axis.title.x=element_blank())+ 
+  theme_bw() +theme(legend.position="none", axis.text.x=element_text(angle=90, size=12), axis.title.x=element_blank())+ 
   scale_fill_manual(values = c("#999999", "#661100", "#0072B2", "#FFB000",  "#FE6100", "#648FFF", "#DC267F" ))
 
 
 plot4=ggboxplot(cross, x = "cs.flag.emergent_overlap" , y = "AI_Mod", fill = "cs.flag.emergent_overlap") +
   #stat_pvalue_manual(stat.test, hide.ns = FALSE) +
-  theme_bw() +theme(legend.position="none", axis.text.x=element_text(angle=90), axis.title.x=element_blank())+ 
+  theme_bw() +theme(legend.position="none", axis.text.x=element_text(angle=90, size=12), axis.title.x=element_blank())+ 
   scale_fill_manual(values = c("#999999", "#661100", "#0072B2", "#FFB000",  "#FE6100", "#648FFF", "#DC267F" ))
 
 
@@ -123,17 +123,17 @@ dunnTest(Mass ~ cs.flag.emergent_overlap, data = cross,   method="holm") #all ar
 #Global satellite - Sed Sat - Water Inbetween   0.5817314  5.607476e-01  5.607476e-01
 
 #Plots with statistics
-plot1a=plot1+annotate("text", x = 1:7, y = 3, label =c("n=666", "n=543", "n=2645", "n=524", "n=1367", "n=1151", "n=881"))
-plot2a=plot2+annotate("text", x = 5, y = 20, label = "a")+annotate("text", x = 6, y = 21, label = "a")
-plot3a=plot3+annotate("text", x = 1, y = 1.1, label = "ab")+annotate("text", x = 2, y = 1.1, label = "a")+
+plot2a=plot2+annotate("text", x = 1:7, y = 3, label =c("n=666", "n=543", "n=2645", "n=524", "n=1367", "n=1151", "n=881"))
+plot3a=plot3+annotate("text", x = 5, y = 20, label = "a")+annotate("text", x = 6, y = 21, label = "a")
+plot4a=plot4+annotate("text", x = 1, y = 1.1, label = "ab")+annotate("text", x = 2, y = 1.1, label = "a")+
 annotate("text", x = 3, y = 1.1, label = "b")
-plot4a=plot4+annotate("text", x = 1, y = 905, label = "a")+annotate("text", x = 4, y = 905, label = "ab")+
+plot1a=plot1+annotate("text", x = 1, y = 905, label = "a")+annotate("text", x = 4, y = 905, label = "ab")+
   annotate("text", x = 2, y = 905, label = "b")+ 
   annotate("text", x = 3, y = 905, label = "c")+ annotate("text", x = 6, y = 905, label = "c")
 
 
 ##Arrange and save it!
-ga=arrangeGrob(plot1a, plot2a, plot3a, plot4a,  ncol=2)
+ga=arrangeGrob(plot1a, plot2a, plot3a, plot4a,  ncol=2, heights=c(3,4.5))
 ggsave("Fig.5_Panel_boxplots_statistics.png", dpi=300, height = 10, width=12, ga)
 
 
